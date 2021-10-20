7627eb0e11b86fa4f0c7b238aeaa10e3fe5059b7
mohanreddy1666 <mohannallamilli11@gmail.com>
2021-10-20 15:16:26 +0200

    code update

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let deckId = ""
+let computerScore = 0
+let myScore = 0
 const newDeck = document.getElementById("new-deck")
 const drawCard = document.getElementById("draw-card")
 const cardsContainer = document.getElementById("cards")
 const winner = document.getElementById("winner")
+const cardCounter = document.getElementById("card-counter")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
 
 function handleClick(){
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
         .then(res => res.json())
         .then(data => {
+            cardCounter.textContent = `Remaining Cards: ${data.remaining}`
             deckId = data.deck_id
         })
 }
@@ -26,6 +32,19 @@ function handleCard(){
             `
             const winnerText = decideWinner(data.cards[0], data.cards[1])
             winner.textContent = winnerText
+            cardCounter.textContent = `Remaining Cards: ${data.remaining}`
+            if (data.remaining === 0) {
+                drawCard.disabled = true
+                if (myScore > computerScore){
+                    winner.textContent = `You Won the Game!`
+                }
+                else if (myScore < computerScore) {   
+                    winner.textContent = `Computer Won the Game!`
+                }
+                else {
+                    winner.textContent = `It's a Tie!`
+                }
+            }
         })
 }
 
@@ -37,9 +56,13 @@ function decideWinner(card1, card2){
     const card1IndexValue = valueOfCards.indexOf(card1.value)
     const card2IndexValue = valueOfCards.indexOf(card2.value)
     if (card1IndexValue > card2IndexValue) {
+        computerScore ++
+        computerScoreEl.textContent = `Computer Score: ${computerScore}`
         return "Computer Wins!"
     }
     else if (card1IndexValue < card2IndexValue) {
+        myScore ++
+        myScoreEl.textContent = `My Score: ${myScore}`
         return "You Win!"
     }
     else {
