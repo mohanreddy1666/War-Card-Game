e5cd033590f6e6590c26e52318e6d4c97125006d
mohanreddy1666 <mohannallamilli11@gmail.com>
2021-10-20 13:50:44 +0200

    code update

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 let deckId = ""
 const newDeck = document.getElementById("new-deck")
 const drawCard = document.getElementById("draw-card")
+const cardsContainer = document.getElementById("cards")
+const winner = document.getElementById("winner")
 
 function handleClick(){
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             deckId = data.deck_id
-        })
-}
-
-function handleCard(){
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.cards)
-            document.getElementById("cards").innerHTML = `
-            <img src=${data.cards[0].image}>
-            <img src=${data.cards[1].image}>
-            `
         })
 }
 
 newDeck.addEventListener("click", handleClick)
 
+function handleCard(){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            cardsContainer.children[0].innerHTML = `
+            <img src=${data.cards[0].image} class="card">
+            `
+            cardsContainer.children[1].innerHTML = `
+            <img src=${data.cards[1].image} class="card">
+            `
+            const winnerText = decideWinner(data.cards[0], data.cards[1])
+            winner.textContent = winnerText
+        })
+}
+
 drawCard.addEventListener("click", handleCard)
+
+function decideWinner(card1, card2){
+    const valueOfCards = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1IndexValue = valueOfCards.indexOf(card1.value)
+    const card2IndexValue = valueOfCards.indexOf(card2.value)
+    if (card1IndexValue > card2IndexValue) {
+        return "Computer Wins!"
+    }
+    else if (card1IndexValue < card2IndexValue) {
+        return "You Win!"
+    }
+    else {
+        return "War!"
+    }    
+}
