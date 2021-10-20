6c3f80f3dd146124aed2276d4dbb41cb34f82682
mohanreddy1666 <mohannallamilli11@gmail.com>
2021-10-20 11:30:08 +0200

    code update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let deckId = ""
 const newDeck = document.getElementById("new-deck")
+const drawCard = document.getElementById("draw-card")
 
 function handleClick(){
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
@@ -10,4 +11,18 @@ function handleClick(){
         })
 }
 
+function handleCard(){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = `
+            <img src=${data.cards[0].image}>
+            <img src=${data.cards[1].image}>
+            `
+        })
+}
+
 newDeck.addEventListener("click", handleClick)
+
+drawCard.addEventListener("click", handleCard)
